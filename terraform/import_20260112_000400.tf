a37f2556290dd293a74d7dbf0b4d9c4012a6ddbb
Nils Müller <tyriis@users.noreply.github.com>
2026-01-12 00:05:14 +0100

    feat(groups): import existing groups

---
+++ terraform/import_20260112_000400.tf
@@ -0,0 +1,19 @@
+import {
+  to = module.groups["filebrowser-admins"].pocketid_group.main
+  id = "6693c447-337c-4e94-9f9f-b97a786c5d56"
+}
+
+import {
+  to = module.groups["filebrowser-users"].pocketid_group.main
+  id = "1330e57c-26cd-4f8d-b33b-2b7ed42d3e5d"
+}
+
+import {
+  to = module.groups["secret-admins"].pocketid_group.main
+  id = "9a8b7767-beaf-42da-a19c-44de3dbe88cc"
+}
+
+import {
+  to = module.groups["secret-users"].pocketid_group.main
+  id = "b0471310-6ee2-4591-bf66-29e8aacda8d2"
+}
