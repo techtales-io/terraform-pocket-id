fa4833fe8115039bd11701f54db092ac1f5d26b8
Nils Müller <tyriis@users.noreply.github.com>
2026-02-20 22:18:07 +0100

    chore(migrations): remove no longer existing references

--- terraform/migrations.tf
+++ terraform/migrations.tf
@@ -1,24 +1,3 @@
-# 20260112_000400 import_existing_groups
-import {
-  to = module.groups["filebrowser-admins"].pocketid_group.main
-  id = "6693c447-337c-4e94-9f9f-b97a786c5d56"
-}
-
-import {
-  to = module.groups["filebrowser-users"].pocketid_group.main
-  id = "1330e57c-26cd-4f8d-b33b-2b7ed42d3e5d"
-}
-
-import {
-  to = module.groups["secret-admins"].pocketid_group.main
-  id = "9a8b7767-beaf-42da-a19c-44de3dbe88cc"
-}
-
-import {
-  to = module.groups["secret-users"].pocketid_group.main
-  id = "b0471310-6ee2-4591-bf66-29e8aacda8d2"
-}
-
 # 20260113_003400 import_existing_clients
 import {
   to = module.clients["filebrowser"].pocketid_client.main
@@ -39,11 +18,6 @@ import {
 import {
   to = module.clients["openbao"].pocketid_client.main
   id = "1c71b01f-6bff-4394-ac10-342e5ea1ff82"
-}
-
-import {
-  to = module.groups["grafana-admins"].pocketid_group.main
-  id = "13e2c6a9-7290-43a5-870d-f4e0b8e2dbbe"
 }
 
 import {
