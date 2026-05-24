8df2ab624050a95a3217c4ac1ff7ebf278ff87e2
Nils Müller <tyriis@users.noreply.github.com>
2026-05-24 23:33:59 +0200

    chore: remove dead code and commented warnings

--- terraform/modules/pocket-id/client/main.tf
+++ terraform/modules/pocket-id/client/main.tf
@@ -24,26 +24,6 @@ resource "pocketid_client" "main" {
   launch_url                = var.config.spec.launchUrl
   requires_reauthentication = var.config.spec.reauthenticate == null ? false : var.config.spec.reauthenticate
   allowed_user_groups       = var.config.spec.groups != null ? sort(var.config.spec.groups) : null
-
-  # │ Warning: Redundant ignore_changes element
-  # │
-  # │   on modules/pocket-id/client/main.tf line 17, in resource "pocketid_client" "main":
-  # │   17: resource "pocketid_client" "main" {
-  # │
-  # │ Adding an attribute name to ignore_changes tells Terraform to ignore future
-  # │ changes to the argument in configuration after the object has been created,
-  # │ retaining the value originally configured.
-  # │
-  # │ The attribute has_logo is decided by the provider alone and therefore there
-  # │ can be no configured value to compare with. Including this attribute in
-  # │ ignore_changes has no effect. Remove the attribute from ignore_changes to
-  # │ quiet this warning.
-
-  #   lifecycle {
-  #     ignore_changes = [
-  #       has_logo,
-  #     ]
-  #   }
 }
 
 resource "vault_generic_secret" "main" {
